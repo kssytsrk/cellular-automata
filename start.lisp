5e569802b219a0c895a81c39a9b847d1c1fe9b1b
kssytsrk <kassy@dismail.de>
2021-02-11 00:10:45 +0200

    some sort of 2d support, not finished, only with von nemann rules

--- start.lisp
+++ start.lisp
@@ -1,10 +1,10 @@
 (in-package #:ca)
 
 (defun start (&key (h 500) (w 500)
-                (ruleset 1))
+                (ruleset 1) (d 1))
   "Start the program."
   (setf *window-width* w)
   (setf *window-height* h)
-  (if (not (setf *ruleset* (get-ruleset ruleset)))
+  (if (not (setf *ruleset* (get-ruleset ruleset d)))
       (format t "Wrong ruleset number input.")
-      (initialize)))
+      (initialize d)))
